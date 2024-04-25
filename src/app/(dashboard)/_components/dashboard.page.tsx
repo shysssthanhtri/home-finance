f8312e8dfe37c066384dc0cf153864f9c349678c
Tri.Thanh.Nguyen <tri.nguyen@Tris-MacBook-Pro.local>
2024-04-25 10:15:37 +0700

    feat: implement dummy chart

--- src/app/(dashboard)/_components/dashboard.page.tsx
+++ src/app/(dashboard)/_components/dashboard.page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { MoneySummaryCard } from "@/app/(dashboard)/_components/money-summary.card";
+import { OverviewChart } from "@/app/(dashboard)/_components/overview-chart";
 import { TransactionItem } from "@/app/(dashboard)/_components/transaction";
 import { DatePickerWithRange } from "@/components/date-range-picker";
 import {
@@ -19,24 +20,29 @@ export const DashboardPage = () => {
         <DatePickerWithRange />
       </Card>
       <div className="flex flex-row gap-6">
-        <div className="flex h-fit shrink-0 basis-1/2 flex-row gap-4">
-          <MoneySummaryCard
-            name="Income"
-            number={12000}
-            lastNumber={10000}
-            differenceMessage="from last day"
-            className="basis-1/2"
-          />
-          <MoneySummaryCard
-            name="Outcome"
-            number={12000}
-            lastNumber={10000}
-            shouldIncrease={false}
-            differenceMessage="from last month"
-            className="basis-1/2"
-          />
+        <div className="shrink-0 basis-1/2 space-y-6">
+          <div className="flex h-fit flex-row gap-4">
+            <MoneySummaryCard
+              name="Income"
+              number={12000}
+              lastNumber={10000}
+              differenceMessage="from last day"
+              className="basis-1/2"
+            />
+            <MoneySummaryCard
+              name="Outcome"
+              number={12000}
+              lastNumber={10000}
+              shouldIncrease={false}
+              differenceMessage="from last month"
+              className="basis-1/2"
+            />
+          </div>
+          <Card className="h-[400px] p-4">
+            <OverviewChart />
+          </Card>
         </div>
-        <Card className="basis-1/2">
+        <Card className="h-fit basis-1/2">
           <CardHeader>
             <CardTitle className="text-base">Transaction list</CardTitle>
             <CardDescription>You have 123 transactions</CardDescription>
