18654926ef65fa5d119e83d2f46e11784fd20240
Tri Nguyen <shyssthanhtri@gmail.com>
2024-04-29 06:38:29 +0700

    refactor: update settings page

--- src/app/settings/_components/settings.page.tsx
+++ src/app/settings/_components/settings.page.tsx
@@ -1,29 +1,18 @@
 import React from "react";
 
-import { PersonalTab } from "@/app/settings/_components/personal.tab";
-import { ProfilesTab } from "@/app/settings/_components/profiles.tab";
-import { SettingsTab, settingsTabs } from "@/app/settings/_configs/tabs";
+import { ThemePicker } from "@/app/settings/_components/theme-picker";
+import { UserInfo } from "@/app/settings/_components/user-info";
 import { PageTitle } from "@/components/page-title";
 import { Card, CardContent } from "@/components/ui/card";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export const SettingsPage = () => {
   return (
     <div className="hidden space-y-6 sm:block">
       <PageTitle title="Settings" />
       <Card className="pt-6">
-        <CardContent className="">
-          <Tabs defaultValue={SettingsTab.PERSONAL}>
-            <TabsList className="mb-4">
-              {settingsTabs.map((tab) => (
-                <TabsTrigger key={tab.value} value={tab.value}>
-                  {tab.title}
-                </TabsTrigger>
-              ))}
-            </TabsList>
-            <PersonalTab />
-            <ProfilesTab />
-          </Tabs>
+        <CardContent className="space-y-4">
+          <UserInfo />
+          <ThemePicker />
         </CardContent>
       </Card>
     </div>
