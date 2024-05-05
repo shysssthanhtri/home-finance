9f9cba3aa958737950b2d514b77e2968179c4c64
Tri Nguyen <shyssthanhtri@gmail.com>
2024-05-05 08:09:22 +0700

    refactor: refactor teams page

--- src/app/(authed)/_components/page-layout.tsx
+++ src/app/(authed)/_components/page-layout.tsx
@@ -3,18 +3,13 @@ import React from "react";
 import { Footer } from "@/app/(authed)/_components/footer";
 import { Header } from "@/app/(authed)/_components/header";
 import { SideBar } from "@/app/(authed)/_components/side-bar";
-import { TeamContextProvider } from "@/app/(authed)/_contexts/team.provider";
-import { getCurrentUser } from "@/server/auth";
-import { api } from "@/trpc/server";
 
 type Props = {
   children: React.ReactNode;
 };
 const PageLayout = async ({ children }: Props) => {
-  const teams = await api.team.getTeams();
-  const user = await getCurrentUser();
   return (
-    <TeamContextProvider teams={teams} user={user}>
+    <>
       <SideBar />
       <div className="flex max-h-screen min-h-screen w-full flex-col overflow-hidden sm:pl-14">
         <Header />
@@ -23,7 +18,7 @@ const PageLayout = async ({ children }: Props) => {
         </div>
         <Footer />
       </div>
-    </TeamContextProvider>
+    </>
   );
 };
 
