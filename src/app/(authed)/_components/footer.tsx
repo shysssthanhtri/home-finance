2b86182f2cae5ad9bbdd4e83e97d1fd4a463c8a0
Tri Nguyen <shyssthanhtri@gmail.com>
2024-05-09 06:40:55 +0700

    feat: update styling for footer

--- src/app/(authed)/_components/footer.tsx
+++ src/app/(authed)/_components/footer.tsx
@@ -1,17 +1,32 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 import { menuItems } from "@/config/menu";
+import { cn } from "@/lib/utils";
 
 export const Footer = () => {
+  const pathName = usePathname();
   return (
     <nav className="flex h-14 w-full items-center justify-around border-t bg-background p-4 sm:hidden">
-      {menuItems.map((item) => (
-        <Link key={item.title} href={item.href}>
-          <item.icon className="h-5 w-5" />
-          <span className="sr-only">{item.title}</span>
-        </Link>
-      ))}
+      {menuItems.map((item) => {
+        const isActive = pathName === item.href;
+        return (
+          <Link
+            key={item.title}
+            href={item.href}
+            className={cn(
+              "text-muted-foreground",
+              isActive && "text-foreground",
+            )}
+          >
+            <item.icon className="h-5 w-5" />
+            <span className="sr-only">{item.title}</span>
+          </Link>
+        );
+      })}
     </nav>
   );
 };
