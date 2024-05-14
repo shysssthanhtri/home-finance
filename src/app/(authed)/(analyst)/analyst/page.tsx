2d63967dcfe7b8e2eb631268dd74383f4731128d
Tri Nguyen <shyssthanhtri@gmail.com>
2024-05-14 10:52:22 +0700

    feat: implement dummy analyst page

---
+++ src/app/(authed)/(analyst)/analyst/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { PageTitle } from "@/app/(authed)/_components/page-title";
+
+const Page = async () => {
+  return (
+    <div className="space-y-4 sm:space-y-6">
+      <PageTitle title="Analyst" />
+    </div>
+  );
+};
+
+export default Page;
